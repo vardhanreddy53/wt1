ac0cb253c051c83d4fd7ba826ee97bc70fffe08c
vardhanreddy53 <vardhanreddy53@gmail.com>
2023-09-15 14:08:05 +0530

    'fifth'

--- uploadfile.php
+++ uploadfile.php
@@ -17,7 +17,7 @@ include('nav.html');
     <div class="box">
         <span class="borderline">
             <form method="post" enctype="multipart/form-data" >
-                <h2>UPLOADING  A FILE</h2>
+                <h3>UPLOADING  A FILE</h3>
                 <div class="inputbox">
                     <input type="file" required="required" name="image">
                     <i></i>
@@ -61,21 +61,18 @@ if (isset($_POST['Upload']))
     $nn=$rename;
 	$tname = $_FILES['image']['tmp_name'];
 	$uploads_dir= "videos/" .$nn;
+    $ft=pathinfo($pname);
+    $f1=$ft['extension'];
 	$t=date('d-m-y')."-".time();
 	if(file_exists($uploads_dir))
 	die('<font color="Crimson" size=5px >'.$pname.' File already Exists</font>');
-	if($_FILES['image']['size']>500000000000)
+	if($_FILES['image']['size']>50000000000)
 	die('<font color="yellow" size=5px>Choose a file size below 5MB</font>');
-	$f1=explode(".",$pname);
-	if(sizeof($f1)>2)
-	{
-		die('<font color="blue" size=5px>Multiple file extensions used</font>');
-		echo "<br>";
-	}
+    $x=0;
 	$ok=array("mkv","mp4","mov");
-	for($i=1;$i<sizeof($f1);$i++)
+	foreach($ok as $a)
 	{
-	if(!in_array($f1[$i],$ok))
+	if(!in_array($f1,$ok))
 		$x=1;
 	}
 	if($x==1)
@@ -86,7 +83,7 @@ if (isset($_POST['Upload']))
 	{
 	move_uploaded_file($tname, $uploads_dir);
 	$sql = "INSERT INTO temp(videos,Uploaded_on) values('$uploads_dir',now())";
-	if(mysqli_query($conn,$sql))
+	// if(mysqli_query($conn,$sql))
 	die('<font color="green" size=5px>'.$pname. ' is Successfully Uploaded</font>');	
 	}
 }
